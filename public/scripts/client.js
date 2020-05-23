582d5d3d7fe1a350df61a7351a686011da5d4a12
Princeton Arulnesan <princeton.arul@gmail.com>
2020-05-23 00:36:36 +0000

    added function to get the age of the tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,15 +29,33 @@
 //
 $(document).ready(function () {
 
-  const escape =  function(str) {
+  const escape = function (str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
+  const howLongAgo = function (date) {
+     if (date < 1000) {
+       return Math.round(date) + " milliseconds ago";
+     } else if (date >= 1000 && date < 60000) {
+       return Math.round((date / 1000)) + "seconds ago";
+     } else if (date >= 60000 && date < 3600000) {
+       return Math.round((date / 60000)) + " minutes ago";
+     } else if (date >= 3600000 && date < 86400000) {
+       return Math.round((date / 3600000)) + " hours ago";
+     } else if (date >= 86400000 && date < 604800000) {
+       return Math.round((date / 86400000)) + " days ago";
+     } else {
+       return Math.round((date/604800000)) + " weeks ago";
+     }
+  }
 
   const createTweetElement = function (tweets) {
-    let date = new Date(tweets.created_at);
-    date = date.toLocaleString();
+    //let date = new Date(tweets.created_at);
+    //date = date.toLocaleTimeString();
+    //date = date - Date.now();
+    let date = (Date.now() - tweets.created_at)
+    let timeElapsed = howLongAgo(date);
     const $tweet = $(`
   <article class="tweet">
     <header class="user">
@@ -52,7 +70,7 @@ $(document).ready(function () {
       <p class="tweet-body">${escape(tweets.content.text)}</p>
     </section>
     <footer class="bottom-bar">
-      <p class="info">${date}</p>
+      <p class="info">${timeElapsed}</p>
       <p class="info">
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
@@ -78,43 +96,55 @@ $(document).ready(function () {
   loadTweets();
 
   $(function () {
+    const $compose = $("#compose");
+    $compose.on("click", function () {
+      //$("#popup").empty();
+      if ($("#popup").first().is(":hidden")) {
+        $("#popup").slideDown(500);
+        $("#tweet-text").focus();
+        event.preventDefault();
+        
+      } else {
+        $("#popup").hide();
+        $(".error").empty();
+        event.preventDefault();
+      }
+    })
+  })
+
+
+  $(function () {
     const $button = $("#submit");
     $button.on("click", function () {
       $(".error").empty();
+      event.preventDefault();
 
       const textLength = ($("textarea")[0].value.length);
       if (textLength === 0) {
         $(".error").prepend("Invalid Tweet");
-        $(".error").slideDown("slow", function(){
-
-        });
-        event.preventDefault();
-        return;
-      } else if (textLength > 140) {
-        $(".error").prepend("Message Too Long");
-        $(".error").slideDown("slow", function(){
-        });
-        event.preventDefault();
-        return;
-      }
-      event.preventDefault();
-      let serial = $("textarea").serialize()
-      $.post("/tweets", serial, function () {
-        //console.log("hello");
-        loadTweet();
-      })
-      $("#tweet-text").val("");
+        $(".error").slideDown(1000);
+      return;
+    } else if (textLength > 140) {
+      $(".error").prepend("Message Too Long");
+      $(".error").slideDown(1000);
+      return;
+    }
+    let serial = $("textarea").serialize()
+    $.post("/tweets", serial, function () {
+      loadTweet();
     })
-    const loadTweet = () => {
-      $.get("/tweets", function (tweetObj) {
-        renderTweet(tweetObj[tweetObj.length-1]);
-      })
-    }
-
-    const renderTweet = function (tweet) {
-        $(".tweets-container").prepend(createTweetElement(tweet));
-    }
+    $("#tweet-text").val("");
   })
+  const loadTweet = () => {
+    $.get("/tweets", function (tweetObj) {
+      renderTweet(tweetObj[tweetObj.length - 1]);
+    })
+  }
+
+  const renderTweet = function (tweet) {
+    $(".tweets-container").prepend(createTweetElement(tweet));
+  }
+})
 })
 
 
